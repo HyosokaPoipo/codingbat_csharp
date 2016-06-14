0a7a396a60c6f52fb39d6a4be4b2e272a157378f
Maryadi <maryadi@elephanttalk.com>
2016-06-14 10:10:07 +0700

    Cleaning master...

--- Codingbat_csharp/Program.cs
+++ Codingbat_csharp/Program.cs
@@ -18,27 +18,7 @@ namespace Codingbat_csharp
 
 
             Console.ReadLine();
-        }
-
-        /*Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power, 
-         * so powerN(3, 2) is 9 (3 squared).
-
-            powerN(3, 1) → 3
-            powerN(3, 2) → 9
-            powerN(3, 3) → 27
-        */
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="baseNum"></param>
-        /// <param name="n"></param>
-        /// <returns></returns>
-    public static int powerN(int baseNum, int n)
-    {
-      if(n == 0 ) return 1;
-      if (n == 1) return baseNum;
-      return baseNum * powerN(baseNum, n - 1);
-    }
+        }      
 
 
  }
