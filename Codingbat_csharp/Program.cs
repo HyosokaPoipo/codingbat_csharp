142414ef81ddd36c79026da2b24e05132252db4a
Maryadi <maryadi@elephanttalk.com>
2016-06-14 14:33:57 +0700

    changePi okiee....

--- Codingbat_csharp/Program.cs
+++ Codingbat_csharp/Program.cs
@@ -14,15 +14,29 @@ namespace Codingbat_csharp
         }
         static void Main(string[] args)
         {
+            String test = "pipi";
+            
+            int index = test.IndexOf("pi");
+            Console.WriteLine("index "+index);
+            Console.WriteLine("Hasil substring "+test.Substring(index+2,test.Length-index-2));
 
-
-
+            Console.WriteLine(changePi("pipi"));
+            Console.WriteLine(changePi("xxpixxpixx"));
+            Console.WriteLine(changePi("xpix"));
             Console.ReadLine();
         }
 
 
 
-
+        public static String changePi(String str)
+        {
+            if (str.Length < 1) return "";
+            int index = str.IndexOf("pi");
+            if (index < 0 ) return str;
+            String temp0 = str.Substring(0, index);
+            String temp = str.Substring(index+2, str.Length-(2+index));
+            return temp0+"3.14" + changePi(temp);
+        }
 
 
 
--- UnitTestCodingBat/UnitTest1.cs
+++ UnitTestCodingBat/UnitTest1.cs
@@ -9,22 +9,28 @@ namespace UnitTestCodingBat
     {
         [TestMethod]
         public void TestMethod1()
-        {        
-            Assert.AreEqual(3, Program.contoh());
+        {
+            Assert.AreEqual("3.14", Program.changePi("pi"));
+            Assert.AreEqual("x3.14x", Program.changePi("xpix"));
+            Assert.AreEqual("3.143.14", Program.changePi("pipi"));
         }
 
 
         [TestMethod]
         public void TestMethod2()
         {
-            
+            Assert.AreEqual("3.14p", Program.changePi("3.14p"));
+            Assert.AreEqual("3.14", Program.changePi("3.14"));
+            Assert.AreEqual("hip", Program.changePi("hip"));
         }
 
 
         [TestMethod]
         public void TestMethod3()
         {
-            
+            Assert.AreEqual("p", Program.changePi("p"));
+            Assert.AreEqual("", Program.changePi(""));
+            Assert.AreEqual("3.14xx", Program.changePi("pixx"));
         }
     }
 }
