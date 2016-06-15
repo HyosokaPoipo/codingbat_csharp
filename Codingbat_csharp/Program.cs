219bb759cf2deca1e09a2e1c367c2ad4b5470056
Maryadi <maryadi@elephanttalk.com>
2016-06-15 09:05:26 +0700

    array6 okiee....

--- Codingbat_csharp/Program.cs
+++ Codingbat_csharp/Program.cs
@@ -21,7 +21,22 @@ namespace Codingbat_csharp
         }
 
 
+        public static Boolean array6(int[] nums, int index)
+        { 
+            //stop condition
+            if (nums.Length == 0) return false;
+            if (nums.Length == 1)
+            {
+                if (nums[0] == 6) return true;
+                else return false;
+            }
+            if(nums[index] == 6) return true;
 
+            //Main recursive process
+            int[] temp = new int[nums.Length-index-1];
+            System.Array.Copy(nums,index+1,temp,0,temp.Length);
+            return array6(temp, index);
+        }
 
 
 
--- UnitTestCodingBat/UnitTest1.cs
+++ UnitTestCodingBat/UnitTest1.cs
@@ -9,22 +9,65 @@ namespace UnitTestCodingBat
     {
         [TestMethod]
         public void TestMethod1()
-        {        
-            Assert.AreEqual(3, Program.contoh());
+        {
+            int[] i = { 1,6,4};
+            Assert.AreEqual(true , Program.array6(i,0));
         }
 
 
         [TestMethod]
         public void TestMethod2()
         {
-            
+            int[] i = { 1, 4 };
+            Assert.AreEqual(false, Program.array6(i, 0));
         }
 
 
         [TestMethod]
         public void TestMethod3()
         {
-            
+            int[] i = { 6 };
+            Assert.AreEqual(true, Program.array6(i, 0));
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] i = {  };
+            Assert.AreEqual(false, Program.array6(i, 0));
+        }
+
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int[] i = { 6, 2, 2 };
+            Assert.AreEqual(true, Program.array6(i, 0));
+        }
+
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int[] i = { 2,5 };
+            Assert.AreEqual(false, Program.array6(i, 0));
+        }
+
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int[] i = { 1, 9, 4, 6, 6 };
+            Assert.AreEqual(true, Program.array6(i, 0));
+        }
+
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            int[] i = { 2, 5, 6 };
+            Assert.AreEqual(true, Program.array6(i, 0));
         }
     }
 }
