0d415ea5fd9d3722c7555d4cc770a32d0dc7396a
Maryadi <maryadi@elephanttalk.com>
2016-06-15 09:44:29 +0700

    array11 okie...

--- Codingbat_csharp/Program.cs
+++ Codingbat_csharp/Program.cs
@@ -21,7 +21,22 @@ namespace Codingbat_csharp
         }
 
 
+        public static int array11(int[] nums, int index)
+        { 
+            //Kondisi stop
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1)
+            {
+                if (nums[0] == 11) return 1;
+                else return 0;
+            }
 
+            //Proses rekursif
+            int[] temp = new int[nums.Length - index - 1];
+            System.Array.Copy(nums, index+1, temp, 0, temp.Length);
+            if (nums[index] == 11) return 1 + array11(temp, index);
+            else return array11(temp, index);
+        }
 
 
 
--- UnitTestCodingBat/UnitTest1.cs
+++ UnitTestCodingBat/UnitTest1.cs
@@ -9,22 +9,65 @@ namespace UnitTestCodingBat
     {
         [TestMethod]
         public void TestMethod1()
-        {        
-            Assert.AreEqual(3, Program.contoh());
+        {
+            int[] i = { 1, 2, 11 };
+            Assert.AreEqual(1, Program.array11(i,0));
         }
-
 
         [TestMethod]
         public void TestMethod2()
         {
-            
+            int[] i = { 11, 11};
+            Assert.AreEqual(2, Program.array11(i, 0));
         }
-
 
         [TestMethod]
         public void TestMethod3()
         {
-            
+            int[] i = { 1,2,3,4,6 };
+            Assert.AreEqual(0, Program.array11(i, 0));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] i = { 1, 11, 3, 11, 11, 13 };
+            Assert.AreEqual(3, Program.array11(i, 0));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int[] i = { 11 };
+            Assert.AreEqual(1, Program.array11(i, 0));
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int[] i = { 1 };
+            Assert.AreEqual(0, Program.array11(i, 0));
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            int[] i = { };
+            Assert.AreEqual(0, Program.array11(i, 0));
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            int[] i = { 11, 2, 3, 4, 11, 5};
+            Assert.AreEqual(2, Program.array11(i, 0));
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            int[] i = { 11, 5, 11 };
+            Assert.AreEqual(2, Program.array11(i, 0));
         }
     }
 }
