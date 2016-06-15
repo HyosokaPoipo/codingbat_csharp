294f3dee58ea18668207011c77322c0dad73655f
Maryadi <maryadi@elephanttalk.com>
2016-06-15 11:10:06 +0700

    endX okiee.....

--- Codingbat_csharp/Program.cs
+++ Codingbat_csharp/Program.cs
@@ -15,13 +15,26 @@ namespace Codingbat_csharp
         static void Main(string[] args)
         {
 
-
-
+            Console.Out.WriteLine(endX("xxre"));
+            Console.Out.WriteLine(endX("xxhixx"));
+            Console.Out.WriteLine(endX("xhixhix"));
+            Console.Out.WriteLine(endX("hellow"));
+            Console.Out.WriteLine(endX(""));
+            Console.Out.WriteLine(endX("x"));
+            Console.Out.WriteLine(endX("e"));
             Console.ReadLine();
         }
 
 
+        public static String endX(String str)
+        {
+            //Kondisi setop
+            if (str.Length < 2) return str;
 
+
+            if (str[0] == 'x') return endX(str.Substring(1)) + str[0];
+            return str[0] + endX(str.Substring(1));
+        }
 
 
 
