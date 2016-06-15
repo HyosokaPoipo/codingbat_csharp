3cd5388a012947d6300b4763c351ee408f7ee720
Maryadi <maryadi@elephanttalk.com>
2016-06-15 10:57:04 +0700

    pairStar okie...

--- Codingbat_csharp/Program.cs
+++ Codingbat_csharp/Program.cs
@@ -15,13 +15,28 @@ namespace Codingbat_csharp
         static void Main(string[] args)
         {
 
-
-
+            Console.WriteLine(pairStar("hellow"));
+            Console.WriteLine(pairStar("xxyy"));
+            Console.WriteLine(pairStar("aaaa"));
+            Console.WriteLine(pairStar(""));
+            Console.WriteLine(pairStar("noadjacent"));
+            Console.WriteLine(pairStar("abba"));
+            Console.WriteLine(pairStar("abbba"));
+            Console.WriteLine(pairStar("112233"));
             Console.ReadLine();
         }
 
 
+        public static String pairStar(String str)
+        { 
+            //Kondisi setop
+            if (str.Length < 2) return str;
 
+
+            //Main rekursif
+            if (str[0] == str[1]) return str[0] + "*" + pairStar(str.Substring(1));
+            else return str[0] + pairStar(str.Substring(1));
+        }
 
 
 
