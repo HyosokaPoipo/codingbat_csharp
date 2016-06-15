59948f391d55d5039b01eb5b528447edc0ebcc43
Maryadi <maryadi@elephanttalk.com>
2016-06-15 11:37:00 +0700

    stringClean okiee...

--- Codingbat_csharp/Program.cs
+++ Codingbat_csharp/Program.cs
@@ -14,13 +14,30 @@ namespace Codingbat_csharp
         }
         static void Main(string[] args)
         {
-
-
-
+            Console.WriteLine(stringClean("yyzzza"));
+            Console.WriteLine(stringClean("abbbcdd"));
+            Console.WriteLine(stringClean("Hellow"));
+            Console.WriteLine(stringClean("XXabcYY"));
+            Console.WriteLine(stringClean("112ab445"));
+            Console.WriteLine(stringClean("Hellow Bookkeeper"));
             Console.ReadLine();
         }
 
+        public static string stringClean(string str)
+        { 
+            //kondisi setop
+            if (str.Length < 2) return str;
 
+            //main rekursif
+            int index = 0;
+            for (index=0; index+1 < str.Length; index++)
+            {
+                if (str[index] != str[index + 1]) break;
+            }
+
+            return str[index] + stringClean(str.Substring(index+1));
+
+        }
 
 
 
